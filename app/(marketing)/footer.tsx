4d0a8ff8a59285fd7deb9cdb6e4ff7c3ca360097
neomat-prog <thegreyliongaming@gmail.com>
2025-03-08 21:16:29 +0100

    add language selection buttons to footer and optimize image loading in marketing page

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,66 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export const Footer = () => {
   return (
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-      Footer
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+        <Button variant={"ghost"} className="w-[180px]">
+          <Image
+            src={"/hr.svg"}
+            alt="Croatian"
+            height={32}
+            width={40}
+            style={{ width: "auto" }}
+            className="mr-2 rounded-md"
+          />
+          Croatian
+        </Button>
+        <Button variant={"ghost"} className="w-[180px]">
+          <Image
+            src={"/es.svg"}
+            alt="Spanish"
+            height={32}
+            width={40}
+            style={{ width: "auto" }}
+            className="mr-2 rounded-md"
+          />
+          Spanish
+        </Button>
+        <Button variant={"ghost"} className="w-[180px]">
+          <Image
+            src={"/fr.svg"}
+            alt="French"
+            height={32}
+            width={40}
+            style={{ width: "auto" }}
+            className="mr-2 rounded-md"
+          />
+          French
+        </Button>
+        <Button variant={"ghost"} className="w-[180px]">
+          <Image
+            src={"/jp.svg"}
+            alt="Japanese"
+            height={32}
+            width={40}
+            style={{ width: "auto" }}
+            className="mr-2 rounded-md"
+          />
+          Japanese
+        </Button>
+        <Button variant={"ghost"} className="w-[180px]">
+          <Image
+            src={"/it.svg"}
+            alt="Italian"
+            height={32}
+            width={40}
+            style={{ width: "auto" }}
+            className="mr-2 rounded-md"
+          />
+          Italian
+        </Button>
+      </div>
     </footer>
   );
 };
