5957a86a6b27b2e6b2050b0a116f2f96d6e15e69
neomat-prog <thegreyliongaming@gmail.com>
2025-03-08 22:22:39 +0100

    add sidebar component with dynamic items and loading indicators

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,5 +1,9 @@
 import { cn } from "@/lib/utils";
-
+import Image from "next/image";
+import Link from "next/link";
+import { SidebarItem } from "./sidebar-item";
+import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
 type Props = {
   className?: string;
 };
@@ -8,11 +12,36 @@ export const Sidebar = ({ className }: Props) => {
   return (
     <div
       className={cn(
-        "flex bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col ",
+        "flex  h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col ",
         className
       )}
     >
-      Sidebar
+      <Link href={"/learn"}>
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src={"/mascot.svg"} height={40} width={40} alt="Mascot" />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Lingo
+          </h1>
+        </div>
+      </Link>
+      <div className="flex flex-col gap-y-2 flex-1">
+        <SidebarItem iconSrc="/learn.svg" label="learn" href="/learn" />
+        <SidebarItem
+          iconSrc="/leaderboard.svg"
+          label="Leaderboard"
+          href="/leaderboard"
+        />
+        <SidebarItem iconSrc="/quests.svg" label="Quests" href="/quests" />
+        <SidebarItem iconSrc="/shop.svg" label="Shop" href="/shop" />
+      </div>
+      <div className="p-4">
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <UserButton />
+        </ClerkLoaded>
+      </div>
     </div>
   );
 };
