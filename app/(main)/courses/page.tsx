194478c39110454a589464bfbc3fbb0e37a9e2d1
neomat-prog <thegreyliongaming@gmail.com>
2025-03-09 19:43:41 +0100

    implement user progress tracking and loading states in courses and learn pages

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,14 +1,24 @@
-import { getCourses } from "@/db/queries";
+import { getCourses, getUserProgress } from "@/db/queries";
 import { List } from "./list";
 
 const CoursesPage = async () => {
-  const courses = await getCourses(); 
-  const sortedCourses = courses.sort((a, b) => a.id - b.id); 
+  const coursesData = await getCourses();
+  const userProgressData = await getUserProgress();
+
+  const [courses, userProgress] = await Promise.all([
+    coursesData,
+    userProgressData,
+  ]);
+
+  const sortedCourses = courses.sort((a, b) => a.id - b.id);
 
   return (
     <div className="h-full max-w-[912px] px-3 mx-auto">
       <h1 className="text-2xl font-bold text-neutral-700">Language Courses</h1>
-      <List courses={sortedCourses} activeCourseId={1} />
+      <List
+        courses={sortedCourses}
+        activeCourseId={userProgress?.activeCourseId}
+      />
     </div>
   );
 };
--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,8 +2,18 @@ import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { Header } from "./header";
 import { UserProgress } from "@/components/user-progress";
+import { getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
 
-const LearnPage = () => {
+const LearnPage = async () => {
+  const userProgressData = getUserProgress();
+
+  const [userProgress] = await Promise.all([userProgressData]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
+
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
@@ -16,7 +26,6 @@ const LearnPage = () => {
       </StickyWrapper>
       <FeedWrapper>
         <Header title="Spanish" />
-        
       </FeedWrapper>
     </div>
   );
