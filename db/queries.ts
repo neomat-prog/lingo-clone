3dd82235e45d5c35c3441e770666ad6d4f325d34
neomat-prog <thegreyliongaming@gmail.com>
2025-03-11 19:33:36 +0100

    add unit retrieval and display in learn page, refactor user progress query

--- db/queries.ts
+++ db/queries.ts
@@ -1,8 +1,8 @@
+import { units, userProgress, courses } from "@/db/schema";
 import { cache } from "react";
 import db from "./drizzle";
 import { auth } from "@clerk/nextjs/server";
 import { eq } from "drizzle-orm";
-import { courses, userProgress } from "./schema";
 
 export const getUserProgress = cache(async () => {
   const { userId } = await auth();
@@ -21,6 +21,43 @@ export const getUserProgress = cache(async () => {
   return data;
 });
 
+export const getUnits = cache(async () => {
+  const userProgress = await getUserProgress();
+  if (!userProgress?.activeCourseId) {
+    return [];
+  }
+  const data = await db.query.units.findMany({
+    where: eq(units.courseId, userProgress.activeCourseId),
+    with: {
+      lessons: {
+        with: {
+          challenges: {
+            with: {
+              challengeProgress: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  const normalizedData = data.map((unit) => {
+    const lessonsWithCompletedStatus = unit.lessons.map((lesson) => {
+      const allCompletedChallenges = lesson.challenges.every((challenge) => {
+        return (
+          challenge.challengeProgress &&
+          challenge.challengeProgress.length > 0 &&
+          challenge.challengeProgress.every((progress) => progress.completed)
+        );
+      });
+      return { ...lesson, completed: allCompletedChallenges };
+    });
+    return { ...unit, lessons: lessonsWithCompletedStatus };
+  });
+
+  return normalizedData;
+});
+
 export const getCourses = cache(async () => {
   const data = await db.query.courses.findMany();
 
@@ -33,4 +70,4 @@ export const getCourseById = cache(async (courseId: number) => {
   });
 
   return data;
-})
+});
