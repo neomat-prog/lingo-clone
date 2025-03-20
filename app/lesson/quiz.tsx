1f427c779c3e5665a0392921a0b7dc581a2d247f
neomat-prog <thegreyliongaming@gmail.com>
2025-03-20 19:33:52 +0100

    add Footer component to quiz and integrate it into the lesson layout

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -5,6 +5,7 @@ import { Header } from "./header";
 import { useState } from "react";
 import { QuestionBubble } from "./question-bubble";
 import { Challenge } from "./challenge";
+import { Footer } from "./footer";
 
 type Props = {
   initialPercentage: number;
@@ -34,8 +35,17 @@ export const Quiz = ({
     return uncompletedIndex === -1 ? 0 : uncompletedIndex;
   });
 
+  const [selectedOption, setSelectedOption] = useState<number>();
+  const [status, setStatus] = useState<"correct" | "wrong" | "none">("none");
+
   const challenge = challenges[activeIndex];
   const options = challenge?.challengeOptions ?? [];
+
+  const onSelect = (id: number) => {
+    if(status !== "none") return;
+
+    setSelectedOption(id)
+  }
 
   const title =
     challenge.type === "ASSIST"
@@ -61,9 +71,9 @@ export const Quiz = ({
               )}
               <Challenge
                 options={options}
-                onSelect={() => {}}
+                onSelect={onSelect}
                 status="none"
-                selectedOption={undefined}
+                selectedOption={selectedOption}
                 disabled={false}
                 type={challenge.type}
               />
@@ -71,6 +81,7 @@ export const Quiz = ({
           </div>
         </div>
       </div>
+      <Footer disabled={!selectedOption} status={status} onCheck={() => {}} />
     </>
   );
 };
