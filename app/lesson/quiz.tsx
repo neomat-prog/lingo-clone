fa193378361a4d2bcdc97f67e3d5a403605504fd
neomat-prog <thegreyliongaming@gmail.com>
2025-03-23 19:45:39 +0100

    add finish state rendering with ResultCard for points and hearts in quiz component

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -10,6 +10,8 @@ import { upsertChallengeProgress } from "@/actions/challenge-progress";
 import { toast } from "sonner";
 import { reduceHearts } from "@/actions/user-progress";
 import { useAudio } from "react-use";
+import Image from "next/image";
+import { ResultCard } from "./result-card";
 
 type Props = {
   initialPercentage: number;
@@ -50,7 +52,6 @@ export const Quiz = ({
 
   const challenge = challenges[activeIndex];
   const options = challenge?.challengeOptions ?? [];
-
 
   const onNext = () => {
     setActiveIndex((current) => current + 1);
@@ -123,8 +124,34 @@ export const Quiz = ({
     }
   };
 
-  if (!challenge) {
-    return <div>Finished the challenge!</div>;
+  if (true || !challenge) {
+    return (
+      <>
+        <div className="flex flex-col gap-y-4 lg:gap-y-8 max-w-lg mx-auto text-center items-center justify-center h-full ">
+          <Image
+            src={"/finish.svg"}
+            alt="Finish"
+            className="hidden lg:block"
+            width={100}
+            height={100}
+          />
+          <Image
+            src={"/finish.svg"}
+            alt="Finish"
+            className="block lg:hidden"
+            width={50}
+            height={50}
+          />
+          <h1 className="text-xl lg:text-3xl font-bold text-neutral-700">
+            Great job! <br /> You&apos;ve completed the lesson
+          </h1>
+          <div className="flex items-center gap-x-4 w-full">
+            <ResultCard variant="points" value={challenges.length * 10} />
+            <ResultCard variant="hearts" value={hearts} />
+          </div>
+        </div>
+      </>
+    );
   }
 
   const title =
