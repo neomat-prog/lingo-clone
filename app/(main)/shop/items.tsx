33e3182449a9e61894da90a8a2049a0284d43806
neomat-prog <thegreyliongaming@gmail.com>
2025-03-24 21:04:20 +0100

    add refillHearts function and integrate it into Items component for heart replenishment

--- app/(main)/shop/items.tsx
+++ app/(main)/shop/items.tsx
@@ -1,7 +1,10 @@
 "use client";
 
+import { refillHearts } from "@/actions/user-progress";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { useTransition } from "react";
+import { toast } from "sonner";
 
 type Props = {
   hearts: number;
@@ -9,7 +12,20 @@ type Props = {
   hasActiveSubscription: boolean;
 };
 
+const POINTS_TO_REFILL = 10;
+
 export const Items = ({ hearts, points, hasActiveSubscription }: Props) => {
+  const [pending, startTransition] = useTransition();
+
+  const onRefillHearts = () => {
+    if (pending || hearts === 5 || points < POINTS_TO_REFILL) {
+      return;
+    }
+
+    startTransition(() => {
+      refillHearts().catch(() => toast.error("Something went wrong!"));
+    });
+  };
   return (
     <ul className="w-full ">
       <div className="flex items-center w-full p-4 gap-x-4 border-t-2">
@@ -19,13 +35,16 @@ export const Items = ({ hearts, points, hasActiveSubscription }: Props) => {
             Refill hearts
           </p>
         </div>
-        <Button disabled={hearts === 5}>
+        <Button
+          onClick={onRefillHearts}
+          disabled={pending || hearts === 5 || points < POINTS_TO_REFILL}
+        >
           {hearts === 5 ? (
             "full"
           ) : (
             <div className="flex items-center">
               <Image src={"./points.svg"} alt="Points" height={20} width={20} />
-              <p>50</p>
+              <p>{POINTS_TO_REFILL}</p>
             </div>
           )}
         </Button>
