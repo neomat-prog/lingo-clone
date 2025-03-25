c1dce0e23dea90c94ddf91cc17055445eda5a29e
neomat-prog <thegreyliongaming@gmail.com>
2025-03-25 20:07:26 +0100

    add user subscription retrieval and Stripe integration in utility functions

--- db/queries.ts
+++ db/queries.ts
@@ -1,10 +1,10 @@
-
 import {
   units,
   userProgress,
   courses,
   challengeProgress,
   lessons,
+  userSubscription,
 } from "@/db/schema";
 import { cache } from "react";
 import db from "./drizzle";
@@ -191,4 +191,27 @@ export const getLessonPercentage = cache(async () => {
     (completedChallenges.length / lesson.challenges.length) * 100
   );
   return percentage;
+});
+
+const DAY_IN_MS = 86_400_000;
+
+export const getUserSubscription = cache(async () => {
+  const { userId } = await auth();
+
+  if (!userId) return null;
+
+  const data = await db.query.userSubscription.findFirst({
+    where: eq(userSubscription.userId, userId),
+  });
+
+  if (!data) return null;
+
+  const isActive =
+    data.stripePriceId &&
+    data.stripeCurrentPeriodEnd?.getTime() + DAY_IN_MS > Date.now();
+
+  return {
+    ...data,
+    isActive: !!isActive,
+  };
 });
