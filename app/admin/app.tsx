169c8f7ab76cf365876bd85f27c1fda0b23b7f30
neomat-prog <thegreyliongaming@gmail.com>
2025-03-31 21:37:59 +0200

    feat: add course creation functionality in admin panel

--- app/admin/app.tsx
+++ app/admin/app.tsx
@@ -3,13 +3,19 @@
 import { Admin, Resource } from "react-admin";
 import simpleRestProvider from "ra-data-simple-rest";
 import { CourseList } from "./course/list";
+import { CourseCreate } from "./course/create";
 
 const dataProvider = simpleRestProvider("/api");
 
 const App = () => {
   return (
     <Admin dataProvider={dataProvider}>
-      <Resource name="courses" recordRepresentation="title" list={CourseList} />
+      <Resource
+        name="courses"
+        recordRepresentation="title"
+        list={CourseList}
+        create={CourseCreate}
+      />
     </Admin>
   );
 };
