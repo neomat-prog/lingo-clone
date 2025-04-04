10390f5486374a91dc55b474c4774abdb0e1d16d
neomat-prog <thegreyliongaming@gmail.com>
2025-04-04 16:37:03 +0200

    fix: update challengeId type from string to number in API routes and ensure proper handling

--- app/api/challenges/[challengeId]/route.ts
+++ app/api/challenges/[challengeId]/route.ts
@@ -7,12 +7,12 @@ import { isAdmin } from "@/lib/admin";
 
 export const GET = async (
   _req: NextRequest,
-  { params }: { params: { challengeId: string } }
+  { params }: { params: { challengeId: number } }
 ) => {
   if (!isAdmin) return new NextResponse("Unauthorized.", { status: 401 });
 
   const data = await db.query.challenges.findFirst({
-    where: eq(challenges.id, Number(params.challengeId)),
+    where: eq(challenges.id, params.challengeId),
   });
 
   return NextResponse.json(data);
@@ -20,7 +20,7 @@ export const GET = async (
 
 export const PUT = async (
   req: NextRequest,
-  { params }: { params: { challengeId: string } }
+  { params }: { params: { challengeId: number } }
 ) => {
   if (!isAdmin) return new NextResponse("Unauthorized.", { status: 401 });
 
@@ -30,7 +30,7 @@ export const PUT = async (
     .set({
       ...body,
     })
-    .where(eq(challenges.id, Number(params.challengeId)))
+    .where(eq(challenges.id, params.challengeId))
     .returning();
 
   return NextResponse.json(data[0]);
@@ -38,13 +38,13 @@ export const PUT = async (
 
 export const DELETE = async (
   _req: NextRequest,
-  { params }: { params: { challengeId: string } }
+  { params }: { params: { challengeId: number } }
 ) => {
   if (!isAdmin) return new NextResponse("Unauthorized.", { status: 401 });
 
   const data = await db
     .delete(challenges)
-    .where(eq(challenges.id, Number(params.challengeId)))
+    .where(eq(challenges.id, params.challengeId))
     .returning();
 
   return NextResponse.json(data[0]);
